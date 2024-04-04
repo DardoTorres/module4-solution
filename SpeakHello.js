ded6dd8c99aae6d21cd16bbcee104b473b4954d8
DardoTorres <164167579+DardoTorres@users.noreply.github.com>
2024-04-04 17:12:39 -0400

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+// By Dardo Torres
+
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function speak(name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
